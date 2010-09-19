d59958c6f2631f3085a69e5eb21891ff2da22707
dsmith <derek@simplegeo.com>
2010-09-19 12:48:10 -0700

    Initial commit.

---
+++ Classes/SGAREngineAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  SGAREngineAppDelegate.h
+//  SGAREngine
+//
+//  Created by Derek Smith on 9/19/10.
+//  Copyright 2010 SimpleGeo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGAREngineAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
